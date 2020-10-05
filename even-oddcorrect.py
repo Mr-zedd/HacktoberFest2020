753a98800f84b8c09a5741e1188dedaa24e49d30
Mr.Zedd <72000565+Mr-zedd@users.noreply.github.com>
2020-10-05 08:09:25 +0530

    Update and rename even-odd.py to even-oddcorrect.py
    Made the program more understandable for a first time user

---
+++ even-oddcorrect.py
@@ -0,0 +1,9 @@
+num=int(input('enter the number'))
+#Enter the no. code is written
+if num%2==0:
+ # checking if divisible by 2
+  print("The no. is even")
+#if even printing
+  else:
+# if odd printing
+print("The no. is odd")
